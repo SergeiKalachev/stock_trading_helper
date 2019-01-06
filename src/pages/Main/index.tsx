37bd579535f1d63864bb1e26372670bb046649c7
SergeiKalachev <Sergei_Kalachev@epam.com>
2019-01-06 18:03:42 +0400

    Main block: markup, styles.

--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -1,7 +1,16 @@
 import * as React from 'react';
 
+import './styles.scss';
+
 export default () => (
     <div className="main">
-        Main page
+        <div className="main__actions">
+            <div className="main__action">
+                Draw Candlestick Chart
+            </div>
+            <div className="main__action">
+                Count Indicators
+            </div>
+        </div>
     </div>
 );
