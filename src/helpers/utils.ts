4675e2633585252dc3c1a0369a017c82fbfe11c5
SergeiKalachev <Sergei_Kalachev@epam.com>
2019-01-11 18:05:46 +0400

    Added function for uuidv4. Draw chart with axes scaling. Reset worksheet removes charts.

--- src/helpers/utils.ts
+++ src/helpers/utils.ts
@@ -19,3 +19,11 @@ export async function wrapExcelLogic(logic) {
         throw error;
     }
 }
+
+// from https://stackoverflow.com/a/2117523
+export function uuidv4() {
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+      var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+      return v.toString(16);
+    });
+}
--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -4,36 +4,57 @@ import { wrapExcelLogic } from '../../helpers/utils'
 
 import './styles.scss';
 
+const CHART_NAME = 'candlestick';
+
 export default class Main extends React.Component {
     handleDrawChartClick = async () => {
         await wrapExcelLogic(async (context) => {
             const dataRange = context.workbook.getSelectedRange();
-            const worksheet = context.workbook.worksheets.getItem("Sheet1");
+            const highColumnIndex = 2;
+            const lowColumnIndex = 3;
+            const highPricesColumn = dataRange.getColumn(highColumnIndex);
+            const lowPricesColumn = dataRange.getColumn(lowColumnIndex);
+            highPricesColumn.load('values');
+            lowPricesColumn.load('values');
+
+            const worksheet = context.workbook.worksheets.getActiveWorksheet();
             const candlestickChart = worksheet.charts.add(Excel.ChartType.stockOHLC, dataRange);
 
+            candlestickChart.name = CHART_NAME;
             candlestickChart.title.text = "Candlesticks";
             candlestickChart.height = 200;
             candlestickChart.width = 500;
             candlestickChart.top = 0;
+            await context.sync();
             
             const verticalAxis = candlestickChart.axes.valueAxis;
-            verticalAxis.maximum = 500;// this.getDataRangeMaximum(verticalAxis.series.getItemAt(0));
-            verticalAxis.minimum = 200;// this.getDataRangeMinimum(verticalAxis.series.getItemAt(0));
-
+            verticalAxis.maximum = Math.max(...this.mapColumnIntoArrayOfValues(highPricesColumn.values));
+            verticalAxis.minimum = Math.min(...this.mapColumnIntoArrayOfValues(lowPricesColumn.values));
             await context.sync();
         });
     }
 
     handleCountIndicatorsClick = () => {}
 
-    getDataRangeMaximum = (series) => {
-        const values = series.points.items;
-        return Math.max(...values);
+    handleResetClick = async () => {
+        await wrapExcelLogic(async (context) => {
+            const worksheet = context.workbook.worksheets.getActiveWorksheet();
+            worksheet.load('charts/items/name');
+            await context.sync();
+
+            for (const chart of worksheet.charts.items) {
+                if (chart.name === CHART_NAME) {
+                    chart.delete();
+                }
+            }
+            await context.sync();
+        });
     }
 
-    getDataRangeMinimum = (series) => {
-        const values = series.points.items;
-        return Math.min(...values);
+    mapColumnIntoArrayOfValues(column) {
+        // this is an array of arrays, where each array consists of only one element
+        // we remove 1st array because it is header
+        return column.slice(1).map(elem => elem[0]);
     }
 
     render() {
@@ -51,6 +72,12 @@ export default class Main extends React.Component {
                         className="main__action"
                     >
                         Count Indicators
+                    </div>
+                    <div
+                        onClick={this.handleResetClick}
+                        className="main__action"
+                    >
+                        Reset worksheet
                     </div>
                 </div>
             </div>
