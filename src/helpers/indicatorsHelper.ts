b9795697be8237c8ecb62f3937d1148ae05a692b
Sergei Kalachev <Sergei_Kalachev@epam.com>
2019-01-16 12:01:00 +0400

    calculateSMA function.

--- src/helpers/indicatorsHelper.ts
+++ src/helpers/indicatorsHelper.ts
@@ -1,4 +1,17 @@
-// calculateSMA
+export function calculateSMA(prices, period) {
+    const SMA = [];
+    for(let i = 0; i < prices.length; i++) {
+        let SMAForPeriod = 0;
+        if (i >= period - 1) {
+            for(let j = i; j >= i - (period - 1); j--) {
+                SMAForPeriod = SMAForPeriod + prices[j];
+            }
+            SMAForPeriod = SMAForPeriod / period;
+        }
+        SMA.push(SMAForPeriod);
+    }
+    return SMA;
+}
 
 // calculateEMA
 
