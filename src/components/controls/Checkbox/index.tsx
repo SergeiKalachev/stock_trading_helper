59fc585a5648e33c6852599fbd87aa2473cc29a9
Sergei Kalachev <Sergei_Kalachev@epam.com>
2019-01-28 17:48:59 +0400

    Changed tab-width for prettier.

--- src/components/controls/Checkbox/index.tsx
+++ src/components/controls/Checkbox/index.tsx
@@ -4,30 +4,30 @@ import cn from 'classnames';
 import './styles.scss';
 
 interface ICheckboxParams {
-  checked?: boolean;
-  onChange?: (...args: any[]) => any;
-  className?: string;
-  type?: string;
+    checked?: boolean;
+    onChange?: (...args: any[]) => any;
+    className?: string;
+    type?: string;
 }
 
 export default ({
-  checked,
-  onChange,
-  className,
-  type = 'default'
+    checked,
+    onChange,
+    className,
+    type = 'default'
 }: ICheckboxParams) => {
-  if (type === 'circle') {
+    if (type === 'circle') {
+        return (
+            <label className={cn('circle__container', className)}>
+                <input type='checkbox' checked={checked} onChange={onChange} />
+                <span className='circle__checkmark' />
+            </label>
+        );
+    }
     return (
-      <label className={cn('circle__container', className)}>
-        <input type='checkbox' checked={checked} onChange={onChange} />
-        <span className='circle__checkmark' />
-      </label>
+        <label className={cn('checkbox2__container', className)}>
+            <input type='checkbox' checked={checked} onChange={onChange} />
+            <span className='checkbox2__checkmark' />
+        </label>
     );
-  }
-  return (
-    <label className={cn('checkbox2__container', className)}>
-      <input type='checkbox' checked={checked} onChange={onChange} />
-      <span className='checkbox2__checkmark' />
-    </label>
-  );
 };
--- src/helpers/utils.ts
+++ src/helpers/utils.ts
@@ -2,59 +2,59 @@ import * as OfficeHelpers from '@microsoft/office-js-helpers';
 
 // returns current server protocol, host, and port like 'protocol://servername:port'  e.g. http://localhost:3000
 export function getServerHost() {
-  let host = '';
-  if (typeof window !== 'undefined' && window.location) {
-    const loc = window.location;
-    host = `${loc.protocol}//${loc.host}`;
-  }
-  return host;
+    let host = '';
+    if (typeof window !== 'undefined' && window.location) {
+        const loc = window.location;
+        host = `${loc.protocol}//${loc.host}`;
+    }
+    return host;
 }
 
 export async function wrapExcelLogic(logic) {
-  try {
-    return await Excel.run(logic);
-  } catch (error) {
-    OfficeHelpers.UI.notify(error);
-    throw error;
-  }
+    try {
+        return await Excel.run(logic);
+    } catch (error) {
+        OfficeHelpers.UI.notify(error);
+        throw error;
+    }
 }
 
 export function stringify(obj) {
-  const getCircularReplacer = () => {
-    const seen = new WeakSet();
-    return (_key, value) => {
-      if (typeof value === 'object' && value !== null) {
-        if (seen.has(value)) {
-          return;
-        }
-        seen.add(value);
-      }
-      return value;
+    const getCircularReplacer = () => {
+        const seen = new WeakSet();
+        return (_key, value) => {
+            if (typeof value === 'object' && value !== null) {
+                if (seen.has(value)) {
+                    return;
+                }
+                seen.add(value);
+            }
+            return value;
+        };
     };
-  };
 
-  return JSON.stringify(obj, getCircularReplacer());
+    return JSON.stringify(obj, getCircularReplacer());
 }
 
 // from https://stackoverflow.com/a/2117523
 export function uuidv4() {
-  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, c => {
-    const r = (Math.random() * 16) | 0,
-      v = c === 'x' ? r : (r & 0x3) | 0x8;
-    return v.toString(16);
-  });
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, c => {
+        const r = (Math.random() * 16) | 0,
+            v = c === 'x' ? r : (r & 0x3) | 0x8;
+        return v.toString(16);
+    });
 }
 
 export function mapColumnIntoArrayOfValues(column) {
-  // this is an array of arrays, where each array consists of only one element
-  // we remove 1st array because it is header
-  return column.slice(1).map(elem => elem[0]);
+    // this is an array of arrays, where each array consists of only one element
+    // we remove 1st array because it is header
+    return column.slice(1).map(elem => elem[0]);
 }
 
 export function getArrayWithMultipleItems(item, countToMultiply) {
-  let result = [];
-  for (let i = 0; i < countToMultiply; i++) {
-    result.push(item);
-  }
-  return result;
+    let result = [];
+    for (let i = 0; i < countToMultiply; i++) {
+        result.push(item);
+    }
+    return result;
 }
