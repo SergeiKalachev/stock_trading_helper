32509b57c691773a2e7a0844534e23216eec2e77
SergeiKalachev <Sergei_Kalachev@epam.com>
2019-03-02 20:35:14 +0400

    Add real gifs.

--- src/pages/Instruction/index.tsx
+++ src/pages/Instruction/index.tsx
@@ -12,25 +12,25 @@ const Instruction: React.FunctionComponent = () => (
                 className='instruction__section'
                 title='Создайте новый лист (Create New Sheet)'
                 bodyText='Структура таблицы будет подготовлена.'
-                imgPath='assets/instructions/test.gif'
+                imgPath='assets/instructions/new_sheet.gif'
             />
             <Section
                 className='instruction__section'
                 title='Вставьте данные'
                 bodyText='В подготовленную таблицу.'
-                imgPath='assets/instructions/test.gif'
+                imgPath='assets/instructions/insert_data.gif'
             />
             <Section
                 className='instruction__section'
                 title='Выделите область'
                 bodyText={`Включая ${TABLE_HEADER.Date}, ${TABLE_HEADER.Open}, ${TABLE_HEADER.High}, ${TABLE_HEADER.Low}, ${TABLE_HEADER.Close}.`}
-                imgPath='assets/instructions/test.gif'
+                imgPath='assets/instructions/select_range.gif'
             />
             <Section
                 className='instruction__section'
                 title='Используйте разные цены'
                 bodyText='Индикаторы можно подсчитать для любой цены. Просто выделите нужную Вам область.'
-                imgPath='assets/instructions/test.gif'
+                imgPath='assets/instructions/various_columns.gif'
             />
             <Section className='instruction__section' title='Цвета сигналов' bodyComponent={Colors} />
         </div>
